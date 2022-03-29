7f90e98d346e7e49f59eda6d34fe8597559367fe
kjanicat <kjanicat@gmail.com>
2022-03-29 22:55:42 +0900

    프로젝트

---
+++ pawintail/src/main/java/com/temkpr/pawintail/PawintailApplication.java
@@ -0,0 +1,13 @@
+package com.temkpr.pawintail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawintailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PawintailApplication.class, args);
+	}
+
+}
---
+++ pawintail/src/test/java/com/temkpr/pawintail/PawintailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.temkpr.pawintail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawintailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
